6646557fc871c9a5ad2d81a1425f93b86b12eefa
Naria-286 <1885397556+Naria-286@users.noreply.github.com>
2024-12-18 02:05:45 -0700

    Create grid but the sizing is wrong

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-let container = document.getElementById("#container")
+let container = document.querySelector("#container")
+
+let gridSize = 10
+let itemSize = (100/gridSize)
+
+function getRandom() {
+    return Math.floor(Math.random() * 256);
+}
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        let gridItem = document.createElement("div")
+        gridItem.classList.add("grid-item")
+        gridItem.classList.add(`${i}-${j}`) // Temp to keep track of grid
+
+        // Currently messing with the sizing of each grid item
+        gridItem.style.cssText = ``
+
+        // Color Differentiation
+        let blue = getRandom()
+        let green = getRandom()
+        let red = getRandom()
+        gridItem.style.backgroundColor = `rgb(${red},${green},${blue})`
+        
+
+        container.appendChild(gridItem)
+    }
+    
+}
