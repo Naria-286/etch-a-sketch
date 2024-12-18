15077557625a13a825d7190704e2e0a8fdf1eb1f
Naria-286 <1885397556+Naria-286@users.noreply.github.com>
2024-12-18 02:27:53 -0700

    Remove Color Randomizer

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ let container = document.querySelector("#container")
 let gridSize = 100
 let itemSize = (100/gridSize)
 
-function getRandom() { //Temp: for random color testing 
-    return Math.floor(Math.random() * 256);
-}
-
 function changeColor (e){
     let element = e.target
     element.classList.add("change-color")
@@ -15,17 +11,11 @@ function changeColor (e){
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         let gridItem = document.createElement("div")
+
         gridItem.classList.add("grid-item")
-        // gridItem.classList.add(`${i}-${j}`) // Temp to keep track of grid
-
-        // Currently messing with the sizing of each grid item
         gridItem.style.cssText = `width:${itemSize}%; height:${itemSize}%`
-
-        // Color Randomization
-        // let blue = getRandom()
-        // let green = getRandom()
-        // let red = getRandom()
-        // gridItem.style.backgroundColor = `rgb(${red},${green},${blue})`
+        // gridItem.classList.add(`${i}-${j}`) // Temp to keep track of grid
+        
         gridItem.addEventListener("mouseover", changeColor)
 
         container.appendChild(gridItem)
